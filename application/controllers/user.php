8ed806523637405610d727e0aa00a4c6a0b2b29b
meglionn <alifyafazilatun@gmail.com>
2025-05-02 01:45:07 +0700

    Tambah controller User buat nampilin daftar pengguna

---
+++ application/controllers/user.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class User extends CI_Controller {
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('User_model');
+    }
+    public function index()
+    {
+        $data['users'] = $this->User_model->get_all_users();
+        $this->load->view('user_list', $data);
+    }
+}
