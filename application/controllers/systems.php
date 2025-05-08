1f1524c219176d48b43ab8667c99f367ad7ff8fe
Achmad-D-Fajar <achmad.dwiky.2405356@students.um.ac.id>
2025-05-09 01:01:49 +0700

    Optimasi ketinggalan

---
+++ application/controllers/systems.php
@@ -0,0 +1,12 @@
+<?php
+
+class systems extends CI_Controller
+{
+  public function index()
+  {
+    $data['title'] = 'Baladhika Majapahit | Sistem & Komposisi';
+    $this->load->view('templates/header', $data);
+    $this->load->view('pages/sistem');
+    $this->load->view('templates/footer');
+  }
+}
