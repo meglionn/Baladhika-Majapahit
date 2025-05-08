fe275c402b12ffdd8eb576050f9638504f1ae56f
Ferdi Dwana <ferdiansyahdwana@gmail.com>
2025-05-09 00:53:44 +0700

    progress page 2 (2)

--- application/views/pages/sistem.php
+++ application/views/pages/sistem.php
@@ -39,7 +39,67 @@
     </div>
   </section>
 
-  <section>
-    <h1>Managerial System & Alur Kegiatan</h1>
+  <section class="managerial-system">
+    <h1 data-aos="fade-up">Managerial System & Alur Kegiatan</h1>
+    <div class="alur-grid">
+      <div class="alur-column-left">
+        <div class="list-kegiatan">
+          <p class="warna1" data-aos="fade-up" data-aos-delay="100">Kontrak Kerjasama</p>
+          <div class="warna1" data-aos="fade-up" data-aos-delay="100">1</div>
+        </div>
+
+        <div class="list-kegiatan">
+          <p class="warna2" data-aos="fade-up" data-aos-delay="100">Kebutuhan SDM</p>
+          <div class="warna2" data-aos="fade-up" data-aos-delay="100">2</div>
+        </div>
+
+        <div class="list-kegiatan">
+          <p class="warna3" data-aos="fade-up" data-aos-delay="100">Rekrutmen dan Seleksi</p>
+          <div class="warna3" data-aos="fade-up" data-aos-delay="100">3</div>
+        </div>
+
+        <div class="list-kegiatan">
+          <p class="warna4" data-aos="fade-up" data-aos-delay="100">Penempatan dan Pemborongan Kerja</p>
+          <div class="warna4" data-aos="fade-up" data-aos-delay="100">4</div>
+        </div>
+
+        <div class="list-kegiatan">
+          <p class="warna5" data-aos="fade-up" data-aos-delay="100">Managerial sistem dan Pengelolaan SDM</p>
+          <div class="warna5" data-aos="fade-up" data-aos-delay="100">5</div>
+        </div>
+      </div>
+
+      <div class="image-column" data-aos="fade-up" data-aos-delay="100">
+        <img src="assets/image/logo/logo.webp" alt="alur kegiatan foto">
+      </div>
+
+      <div class="alur-column-right">
+        <div class="list-kegiatan">
+          <div class="warna5" data-aos="fade-up" data-aos-delay="100">6</div>
+          <p class="warna5" data-aos="fade-up" data-aos-delay="100">Wajib lapor Tenaga Kerja di Disnaker</p>
+        </div>
+
+        <div class="list-kegiatan">
+          <div class="warna4" data-aos="fade-up" data-aos-delay="100">7</div>
+          <p class="warna4" data-aos="fade-up" data-aos-delay="100">Pengelolaan Asuransi Tenaga Kerja</p>
+        </div>
+
+        <div class="list-kegiatan">
+          <div class="warna3" data-aos="fade-up" data-aos-delay="100">8</div>
+          <p class="warna3" data-aos="fade-up" data-aos-delay="100">Pengelolaan Penggajian dan Lembur</p>
+        </div>
+
+        <div class="list-kegiatan">
+          <div class="warna2" data-aos="fade-up" data-aos-delay="100">9</div>
+          <p class="warna2" data-aos="fade-up" data-aos-delay="100">Pengelolaan Etos Kerja SDM/ Pendidikan dan Latihan</p>
+        </div>
+
+        <div class="list-kegiatan">
+          <div class="warna1" data-aos="fade-up" data-aos-delay="100">10</div>
+          <p class="warna1" data-aos="fade-up" data-aos-delay="100">Evaluasi Prestasi Kerja</p>
+        </div>
+      </div>
+    </div>
+
   </section>
 </main>
