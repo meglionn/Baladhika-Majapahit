95db20cbba63685848afd9ca83f65fed27fea87b
meglionn <alifyafazilatun@gmail.com>
2025-05-09 22:29:37 +0700

    update

--- application/controllers/profile.php
+++ application/controllers/profile.php
@@ -1,13 +1,12 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Profile extends CI_Controller
 {
   public function index()
   {
-    $data['title'] = 'Baladhika Majapahit | Profil';
+    $data['title'] = 'Baladhika Majapahit | Profile';
     $this->load->view('templates/header', $data);
-    $this->load->view('pages/profile', $data);
+    $this->load->view('pages/profile');
     $this->load->view('templates/footer');
   }
 }
