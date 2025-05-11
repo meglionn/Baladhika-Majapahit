d952fb817a4d5c35466f0e9a4e870dedf5a4844d
meglionn <alifyafazilatun@gmail.com>
2025-05-11 21:44:45 +0700

    update footer style

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,41 +1,30 @@
-<footer class="footer">
-        <div class="footer-top" data-aos="fade-left">
-            <div class="col-md-3 text-center mb-3">
-                <img src="<?= base_url('assets/img/logo-footer.png') ?>" alt="Logo Baladhika" style="max-height: 80px;">
-            </div>
-            <div class="office">
-                <h2>HEAD OFFICE</h2>
-                <ul>
-                    <li>Jl. Raya Sambiroto</li>
-                    <li>KM. 04 Griya Kaliandra No. 16A</li>
-                    <li>Dsn. Sambiroto Ds. Mlaten</li>
-                    <li>Kec. Puri Kab. Mojokerto (61363)</li>
-                </ul>
-            </div>
+<footer class="bg-dark text-white pt-4 pb-2 mt-5">
+  <div class="container">
+    <div class="row align-items-center">
 
-            <div class="contact">
-                <h2>CONTACT OFFICE</h2>
-                <ul>
-                    <li>(0321) 5884552</li>
-                    <li>+62813 33196668</li>
-                    <li>@baladhikamajapahit</li>
-                    <li>baladhikagrub@gmail.com</li>
-                    <li>baladhikamajapahit88@yahoo.com</li>
-                </ul>
-            </div>
-        </div>
+      <div class="col-md-3 text-center mb-3">
+        <img src="<?= base_url('assets/image/logo/logo.webp') ?>" alt="Logo Baladhika" style="max-height: 80px;">
+      </div>
 
+      <div class="col-md-4">
+        <h5 class="text-warning">HEAD OFFICE</h5>
+        <p class="mb-1">Jl. Raya Sambiroto KM. 04 Griya Kaliandra No. 16A</p>
+        <p class="mb-1">Ds. Mlaten, Kec. Puri, Kab. Mojokerto (61363)</p>
+      </div>
 
-        <div class="footer-bottom">
-            <div class="container">
-                <p>&copy; 2025 Baladhika Majapahit. All rights reserved.</p>
-            </div>
-        </div>
-    </footer>
+      <div class="col-md-5">
+        <h5 class="text-warning">CONTACT OFFICE</h5>
+        <p class="mb-1">Telp: (0321) 5884552</p>
+        <p class="mb-1">HP: +62 813-3196-6668</p>
+        <p class="mb-1">@baladhikamajapahit</p>
+        <p class="mb-1">baladhikagrub@gmail.com</p>
+        <p>baladhikamajapahit88@yahoo.com</p>
+      </div>
+    </div>
 
-    <button id="scrollToTop" aria-label="Scroll to top">
-        <b class="fas fa-arrow-up">&#8593;</b>
-    </button>
-</body>
- 
-</html>
+    <hr class="bg-light">
+    <div class="text-center">
+      <small>&copy; 2025 <strong>Baladhika Majapahit</strong>. All rights reserved.</small>
+    </div>
+  </div>
+</footer>
