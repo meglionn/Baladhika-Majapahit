00790ff2c170c037f2ac07825daa14254467abb0
meglionn <alifyafazilatun@gmail.com>
2025-05-13 16:37:59 +0700

    update footer

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,9 +1,28 @@
-<footer class="bg-dark text-white pt-4 pb-2 mt-5">
+<style>
+    .bg-blue-cool {
+      width: 70%;
+      margin-left: auto;
+      background: #26b1c1;
+      box-shadow: 0 4px 20px rgba(0, 0, 0, 0.2);
+      padding: 2rem; 
+      border-radius: 0.5rem;
+    }
+</style>
+  <footer class="bg-blue-cool text-white mt-5" data-aos="fade-up">
   <div class="container">
     <div class="row align-items-center">
 
-      <div class="col-md-3 text-center mb-3">
-        <img src="<?= base_url('assets/image/logo/logo.webp') ?>" alt="Logo Baladhika" style="max-height: 80px;">
+      <div class="col-md-3 d-flex justify-content-center mb-3">
+        <img src="<?= base_url('assets/image/logo/logo.webp') ?>" alt="Logo Baladhika" style="max-height: 100px;">
+      </div>
+
+      <div class="col-md-4">
+        <h5 class="text-warning">CONTACT OFFICE</h5>
+        <p class="mb-1">Telp: (0321) 5884552</p>
+        <p class="mb-1">HP: +62 813-3196-6668</p>
+        <p class="mb-1">@baladhikamajapahit</p>
+        <p class="mb-1">baladhikagrub@gmail.com</p>
+        <p>baladhikamajapahit88@yahoo.com</p>
       </div>
 
       <div class="col-md-4">
@@ -12,14 +31,6 @@
         <p class="mb-1">Ds. Mlaten, Kec. Puri, Kab. Mojokerto (61363)</p>
       </div>
 
-      <div class="col-md-5">
-        <h5 class="text-warning">CONTACT OFFICE</h5>
-        <p class="mb-1">Telp: (0321) 5884552</p>
-        <p class="mb-1">HP: +62 813-3196-6668</p>
-        <p class="mb-1">@baladhikamajapahit</p>
-        <p class="mb-1">baladhikagrub@gmail.com</p>
-        <p>baladhikamajapahit88@yahoo.com</p>
-      </div>
     </div>
 
     <hr class="bg-light">
