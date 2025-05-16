95ce7285ff90ed9e4cc2239f1ea5244ac333b6b3
Achmad-D-Fajar <achmad.dwiky.2405356@students.um.ac.id>
2025-05-16 07:24:03 +0700

    CREATE and DELETE tabel migas
    Menambah halaman CRUD, pada page profil untuk tambah dan hapus mitra migas, dan nyicil menambah mitra

--- application/models/modelMigas.php
+++ application/models/modelMigas.php
@@ -4,4 +4,16 @@ class modelMigas extends CI_Model{
     public function getAllMigas(){
         return $this->db->get('migas')->result_array();;
     }
+
+    public function addMigas(){
+        $data = [
+            "namaGas" => $this->input->post('namaGas', true)
+        ];
+        $this->db->insert('migas', $data);
+    }
+
+    public function deleteMigas($id){
+        $this->db->where('idGas', $id);
+        $this->db->delete('migas');
+    }
 }
--- application/models/modelMitra.php
+++ application/models/modelMitra.php
@@ -4,4 +4,17 @@ class modelMitra extends CI_Model{
     public function getAllMitra(){
         return $this->db->get('mitra')->result_array();;
     }
+    
+    public function addMitra(){
+        $data = [
+            "logo" => $this->input->post('logo', true),
+            "namaMitra" => $this->input->post('namaMitra', true)
+         ];
+         $this->db->insert('mitra', $data);
+    }
+
+    public function deleteMitra($id){
+        $this->db->where('idMitra', $id);
+        $this->db->delete('mitra');
+    }
 }
