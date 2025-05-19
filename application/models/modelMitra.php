6728edf7359b63e04fb3ef8be9d20aa2c02c8da1
Achmad-D-Fajar <achmad.dwiky.2405356@students.um.ac.id>
2025-05-19 23:30:21 +0700

    Add and Delete now Functional
    Gabungkan form add ke index, fungsi tambah dan hapus mitra sekarang fungional

--- application/models/modelMitra.php
+++ application/models/modelMitra.php
@@ -1,27 +1,33 @@
 <?php
-
 class modelMitra extends CI_Model{
-
-     public function __construct()
-    {
+    public function __construct() {
         parent::__construct();
         $this->load->database();
     }
     
     public function getAllMitra(){
-        return $this->db->get('mitra')->result_array();;
+        return $this->db->get('mitra')->result_array();
     }
-    
-    public function addMitra(){
+
+    public function getMitraById($id) {
+        return $this->db->get_where('mitra', ['idMitra' => $id])->row_array();
+    }
+
+    public function addMitra($filename) {
         $data = [
-            "logo" => $this->input->post('logo', true),
+            "logo" => $filename,
             "namaMitra" => $this->input->post('namaMitra', true)
-         ];
-         $this->db->insert('mitra', $data);
+        ];
+        return $this->db->insert('mitra', $data);
     }
 
     public function deleteMitra($id){
         $this->db->where('idMitra', $id);
-        $this->db->delete('mitra');
+        return $this->db->delete('mitra');
+    }
+    
+    public function updateMitra($id, $data) {
+        $this->db->where('idMitra', $id);
+        return $this->db->update('mitra', $data);
     }
 }
